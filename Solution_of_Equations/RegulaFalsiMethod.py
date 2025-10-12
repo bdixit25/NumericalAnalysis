e652a183e18b56761cf90bbac0d534b72d4a6eee
bdixit <bhupesh.dakshana16@gmail.com>
2025-10-12 19:21:58 +0200

    Minimum working example of RegulaFalsiMethod.

---
+++ Solution_of_Equations/RegulaFalsiMethod.py
@@ -0,0 +1,32 @@
+from common.calculation_tools import critical_range_finder, eval_equation
+
+
+
+def iterator_function(x1, y1, x2, y2, equation, iter_count = 0, max_iter = 100):
+    x = (x2*y1 - x1*y2)/(y1 - y2)
+    y = eval_equation(equation, x)
+
+    if iter_count == max_iter:
+        return x, y
+
+    return iterator_function(x, y, x2, y2, equation, iter_count+1)
+    
+
+def solution_finder(equation):
+    roots = []
+
+    critical_ranges = critical_range_finder(equation)
+
+    for range in critical_ranges:
+        x_begin = range[0][0]
+        x_end = range[1][0]
+
+        x_new, y_new = iterator_function(x_begin, eval_equation(equation, x_begin), x_end, eval_equation(equation, x_end), equation)
+        roots.append((x_new, y_new))
+    
+    print(roots)
+    return roots
+
+if __name__ == '__main__':
+    solution_finder(equation)
+    
---
+++ common/calculation_tools.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+from itertools import pairwise
+
+def eval_equation(equation, x = None):
+    try:
+        return eval(equation) 
+    except:
+        print("You must provide the value at which th equation has to be evaluated.")
+    #return 2*x - math.log10(x) - 7
+
+
+# Simple function to find the values of the given function in a given range
+def value_finder(equation, start = -1000, stop = 1000, step = 0.1):
+    x_values = np.arange(start, stop, step)
+    y_values = []
+
+    for x in x_values:
+        y_val = eval_equation(equation, x)
+        y_values.append(y_val)
+
+    val_list = list(zip(x_values, y_values))
+    return val_list 
+
+# Function to find the ranges where roots might exist
+def critical_range_finder(equation):
+    critical_ranges = []
+    values = value_finder(equation)
+
+    for a, b in pairwise(values):
+        if a[1]*b[1] < 0:
+            critical_ranges.append((a, b))
+    return critical_ranges
